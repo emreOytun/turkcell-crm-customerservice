aa9c080a7804820a46b5861c1bd8e87dc62b56de
emreOytun <emreoytun58@gmail.com>
2024-09-18 00:08:51 +0300

    log4j2 logging added

--- src/main/java/com/turkcell/pair3/customerservice/configuration/SecurityConfiguration.java
+++ src/main/java/com/turkcell/pair3/customerservice/configuration/SecurityConfiguration.java
@@ -19,7 +19,8 @@ public class SecurityConfiguration {
         baseSecurityService.configureCoreSecurity(http);
 
         http.authorizeHttpRequests(
-                req->req.requestMatchers("/api/**").hasAnyAuthority("admin")
+//                req->req.requestMatchers("/api/**").hasAnyAuthority("admin")
+                  req->req.requestMatchers("/api/**").authenticated()
         );
 
         return http.build();
